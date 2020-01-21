6a9921dc5da419e7b1d2af5b624d014c9510a66d
pocketdru <pocketdru@gmail.com>
2020-01-20 22:06:35 -0700

    favicon removed

---
+++ client/build/precache-manifest.9ac84c12406cd09766ac17635f54ce88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1deb83206999daa02417c8e56bdbbab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a820a8461f5a1c02663a",
+    "url": "/static/css/main.b01234bf.chunk.css"
+  },
+  {
+    "revision": "05e97750f5f351921bf5",
+    "url": "/static/js/2.b1e99726.chunk.js"
+  },
+  {
+    "revision": "a820a8461f5a1c02663a",
+    "url": "/static/js/main.beaebbd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec060802de64b44b6bca5af2191d2381",
+    "url": "/static/media/grilled-salmon-steak-is-ready-picjumbo-com.ec060802.png"
+  },
+  {
+    "revision": "0ffb3a345eaf2421e265cd252d26d8be",
+    "url": "/static/media/healthy-pancakes-with-cottage-cheese-and-blueberries-2-picjumbo-com.0ffb3a34.png"
+  },
+  {
+    "revision": "cb2eda346878fa27066f7ac1fc889f43",
+    "url": "/static/media/max_fresh-baguette-morning-breakfast-picjumbo-com.cb2eda34.png"
+  },
+  {
+    "revision": "d77b9c739effc7df20fcaed09637f4b3",
+    "url": "/static/media/preparing-salami-pizza-picjumbo-com.d77b9c73.png"
+  },
+  {
+    "revision": "0cc8111f42ba2e958da5c4667bb725f3",
+    "url": "/static/media/preparing-salmon-steak-close-up-picjumbo-com.0cc8111f.png"
+  },
+  {
+    "revision": "95d65351ebb26a5964f6165fed2a67da",
+    "url": "/static/media/salad-bowl-with-mozzarella-and-cherry-tomatoes-picjumbo-com.95d65351.png"
+  },
+  {
+    "revision": "10f130ae89722b0a08ce79e4bf456211",
+    "url": "/static/media/sweet-morning-breakfast-picjumbo-com.10f130ae.png"
+  },
+  {
+    "revision": "86ec53b33439ee2d6edf1111e4cd75ab",
+    "url": "/static/media/yummy-chocolate-waffles-from-open-air-food-market-picjumbo-com.86ec53b3.png"
+  }
+]);
