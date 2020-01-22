059fb55364b9c3ef0eb5ec7d55312feac37cd7b2
pocketdru <pocketdru@gmail.com>
2020-01-21 21:08:59 -0700

    heroku

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = require("express").Router();
 
 
 // If no API routes are hit, send the React app
-router.use("/", "../client/build/index.html");
+router.get("/", function (req, res) {
+        res.render("index.html");
+});
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Use apiRoutes
-// app.use("/api", apiRoutes);
-app.use(routes);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./build/public/index.html"));
+//   });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
